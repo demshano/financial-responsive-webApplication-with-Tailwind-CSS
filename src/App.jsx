c9634d464d668a88a3c02a4269226942062ab020
demshano <delabro99@gmail.com>
2023-11-13 18:56:19 +0530

    started to design financial responsive web page with the power of Tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavBar } from './components/NavBar'
+import { Hero } from './components/Hero'
+import { Analytics } from './components/Analytics'
+
+export const App = () => {
+  return (
+  
+    <div>
+      <NavBar />
+      <Hero />
+      <Analytics />
+    </div>
+
+  )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Typed from 'react-typed'
+
+export const Hero = () => {
+  return (
+    <div className='text-white'>
+        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center '>
+            <p className='uppercase text-[#00df9a] font-bold p-2'>growing with data analytics.</p>
+            <h1 className='capitalize md:text-7xl sm-text-6xl text-4xl md:py-6'>grow with data</h1>
+            <div className='flex justify-center py-4'>
+                <p className='md:text-5xl sm:text-4xl text-xl font-bold '>fast, flexible finance for</p>
+                <Typed 
+                className='md:text-5xl sm:text-4xl text-xl font-bold pl-2'
+                strings={['BTB', 'BTC', 'SAAS']} 
+                typeSpeed={120} 
+                backSpeed={140} 
+                loop
+                />
+            </div>
+            <p className='md:text-2xl text-xl font-bold text-gray-500 capitalize'>monitor your data analitics to increase revenue for BTS, BTC and SAAS platforms.</p>
+            <button className='capitalize w-[200px] bg-[#00df9a] font-medium my-6 py-3 rounded-md mx-auto'>get started</button>
+        </div>
+    </div>
+  )
+}
